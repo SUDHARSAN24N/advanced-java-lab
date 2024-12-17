2f98f4397fa9ca8278e71907de2af7dfbad2d299
N.Sudharsan <148472454+SUDHARSAN24N@users.noreply.github.com>
2024-12-16 21:10:20 -0800

    fix:Color Add to text

--- pg1.java
+++ pg1.java
@@ -1,3 +1,4 @@
+
 import java.applet.*;
 import java.awt.*;
 public class pg1 extends Applet
@@ -8,15 +9,19 @@ g.drawLine(100,100,500,100);
 g.drawLine(100,100,100,500);
 g.drawLine(100,500,500,500);
 g.drawLine(500,500,500,100);
-g.drawString("I.M.SC",280,130);
 g.drawLine(150,150,450,150);
-g.drawString("welcome",180,230);
-g.drawString("to",300,300);
-g.drawString("all",380,380); 
 g.drawLine(150,150,150,450);
 g.drawLine(150,450,450,450);
 g.drawLine(450,450,450,150);
+g.setColor(Color.red);
+g.drawString("I.M.SC",280,130);
 g.drawString("ComputerScience",250,480);
+g.setColor(Color.blue);
+g.drawString("welcome",180,230);
+g.drawString("to",300,300);
+g.drawString("all",380,380); 
+
+
 }
 }
 //<applet code="pg1.class" width=1000 height=1000></applet>
