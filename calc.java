0570c905b8dbb61de3d2095d0c6cc39794440a29
N.Sudharsan <148472454+SUDHARSAN24N@users.noreply.github.com>
2025-01-08 21:09:36 -0800

    fix:change postion

--- calc.java
+++ calc.java
@@ -6,61 +6,62 @@ public class calc extends Applet implements ActionListener{
         Button b0,b1,b2,b3,b4,b5,b6,b7,b8,b9;
         Button add,sub,mul,div,dot,c,equ;
         String msg="",tmp;
-        int a,b;
+        Double a,b;
     public void init(){ 
+        setLayout(null);
         t1=new TextField();
-        t1.setBounds(15,15,150,30);
+        t1.setBounds(15,15,135,30);
         t1.addActionListener(this);
         b9=new Button("9");
-        b9.setBounds(18,50,32,32);
+        b9.setBounds(18,50,42,42);
         b9.addActionListener(this);
         b8=new Button("8");
-        b8.setBounds(53,50,32,32);
+        b8.setBounds(63,50,42,42);
         b8.addActionListener(this);
         b7=new Button("7");
-        b7.setBounds(88,50,32,32);
+        b7.setBounds(108,50,42,42);
         b7.addActionListener(this);
         b6=new Button("6");
-        b6.setBounds(18,85,32,32);
+        b6.setBounds(18,95,42,42);
         b6.addActionListener(this);
         b5=new Button("5");
-        b5.setBounds(53,85,32,32);
+        b5.setBounds(63,95,42,42);
         b5.addActionListener(this);
         b4=new Button("4");
-        b4.setBounds(88,85,32,32);
+        b4.setBounds(108,95,42,42);
         b4.addActionListener(this);
         b3=new Button("3");
-        b3.setBounds(18,118,32,32);
+        b3.setBounds(18,138,42,42);
         b3.addActionListener(this);
         b2=new Button("2");
-        b2.setBounds(53,118,32,32);
+        b2.setBounds(63,138,42,42);
         b2.addActionListener(this);
         b1=new Button("1");
-        b1.setBounds(88,118,32,32);
+        b1.setBounds(108,138,42,42);
         b1.addActionListener(this);
         add=new Button("+");
-        add.setBounds( 129, 50, 32, 32);
+        add.setBounds(153, 50, 42, 42);
         add.addActionListener(this);
         sub=new Button("-");
-        sub.setBounds(129,85,32,32);
+        sub.setBounds(153,95,42,42);
         sub.addActionListener(this);
         mul=new Button("*");
-        mul.setBounds(129,118,32,32);
+        mul.setBounds(153,138,42,42);
         mul.addActionListener(this);
         div=new Button("/");
-        div.setBounds(129,152,32,32);
+        div.setBounds(153,182,42,42);
         div.addActionListener(this);
         dot=new Button(".");
-        dot.setBounds(18,152,32,32);
+        dot.setBounds(18,182,42,42);
         dot.addActionListener(this);
         b0=new Button("0");
-        b0.setBounds(53,152,32,32);
+        b0.setBounds(63,182,42,42);
         b0.addActionListener(this);
         equ=new Button("=");
-        equ.setBounds(128,152,32,32);
+        equ.setBounds(108,182,42,42);
         equ.addActionListener(this);
         c=new Button("c");
-        c.setBounds(180,180,32,32);
+        c.setBounds(160,15,32,32);
         c.addActionListener(this);
         add(t1);
         add(b9);
@@ -87,7 +88,7 @@ public class calc extends Applet implements ActionListener{
         {
         String s1=t1.getText();
         tmp=s;
-        a=Integer.parseInt(s1);
+        a=Double.parseInt(s1);
         msg="";
         }
         else if(s.equals("="))
@@ -103,11 +104,12 @@ public class calc extends Applet implements ActionListener{
                 sum=a*b;
             else if(tmp=="/")
                 sum=a/b;
+            String s1=String.valueOf(sum);
+            t1.setText(""+s1);
         }
-       else if(s.equals("c"))
-        {
+        else if(s.equals("c")){
             t1.setText("");
-      }
+        }
 else{
 msg+=s;
 t1.setText(msg);
@@ -115,8 +117,8 @@ t1.setText(msg);
 }
    public void paint(Graphics g)
 {
- 	g.setColor(Color.red);
-        g.drawRect(10,10,185,185);        
+ 	g.setColor(Color.gray);
+        g.fillRect(10,10,190,220);        
 }
 }
-//<appletviewer code="calc.class" width="350" height="350"></applet>
+//<appletviewer code="calc.class" width="370" height="370"></applet>
